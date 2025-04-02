b8bb908097237d431cc850a780eef570321cb124
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-04-01 22:38:16 -0300

    Adicionando exercicio do 1 ao 7

---
+++ Lista 2/Exercício 1 ao 5 - Lista 2.py
@@ -0,0 +1,19 @@
+lista = [1,2,3,4,5]
+
+print (lista)
+
+lista.append(10)
+
+print(lista)
+
+del lista[2]
+
+print(lista)
+
+tamanho_lista = len(lista)
+
+print(f'O tamanho da lista é {tamanho_lista}.')
+
+print(lista[::-1]) #exibe a lista em sentido inverso
+
+   
